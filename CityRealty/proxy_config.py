7daf146214c4a665530915e27cefbc1e51dc9a35
Magnus Xu <31779143+MagnusXu@users.noreply.github.com>
2019-04-10 11:13:23 -0400

    Update proxy_config.py

--- CityRealty/proxy_config.py
+++ CityRealty/proxy_config.py
@@ -13,20 +13,19 @@ proxy_dic_list = {}
 num_lines = 0
 # Save all proxy object in proxy_dic_list
 with open('proxy-list.txt') as source:
-	for line in source:
-		data = json.loads(line.read())
-		host = data["host"]
-		http_proxy = "http://" + host + ":80"
-		https_proxy = "https://" + host + ":443"
-		ftp_proxy = "ftp://" + host + ":21"
-		proxy_dict = {
-			"http" : http_proxy,
-			"https" : https_proxy,
-			"ftp" : ftp_proxy
-		}
+    for line in source:
+        data = json.load(line)
+        host = data['host']
+        http_proxy = "http://" + host + ":80"
+        https_proxy = "https://" + host + ":443"
+        ftp_proxy = "ftp://" + host + ":21"
+        proxy_dict = {
+                "http": http_proxy,
+                "https": https_proxy,
+                "ftp": ftp_proxy
+        }
         print(proxy_dict)
-        num_lines += 1 # Count the total line of this file
-		proxy_dic_list[num_lines].append(proxy_dict)
-
-
+        num_lines += 1
+        proxy_dic_list[num_lines].append(proxy_dict)
+	
 r = requests.get(url, headers = headers, proxies = proxy_dict)
