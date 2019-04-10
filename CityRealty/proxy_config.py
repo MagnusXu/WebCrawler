ccb63c32bb125b49c815ba2e500d08debc1d7814
Magnus Xu <31779143+MagnusXu@users.noreply.github.com>
2019-04-10 11:04:11 -0400

    Create proxy_config.py

---
+++ CityRealty/proxy_config.py
@@ -0,0 +1,32 @@
+import requests
+import re
+import json
+
+url = "https://www.cityrealty.com/"
+headers = {
+	"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
+	"Cookie":""
+	# I don't know where you store cookies
+}
+
+proxy_dic_list = {}
+num_lines = 0
+# Save all proxy object in proxy_dic_list
+with open('proxy-list.txt') as source:
+	for line in source:
+		data = json.loads(line.read())
+		host = data["host"]
+		http_proxy = "http://" + host + ":80"
+		https_proxy = "https://" + host + ":443"
+		ftp_proxy = "ftp://" + host + ":21"
+		proxy_dict = {
+			"http" : http_proxy,
+			"https" : https_proxy,
+			"ftp" : ftp_proxy
+		}
+        print(proxy_dict)
+        num_lines += 1 # Count the total line of this file
+		proxy_dic_list[num_lines].append(proxy_dict)
+
+
+r = requests.get(url, headers = headers, proxies = proxy_dict)
