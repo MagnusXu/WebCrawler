648c9b8665626f31c77fbf22a186cd05f17dc8e3
Magnus Xu <31779143+MagnusXu@users.noreply.github.com>
2019-04-17 11:57:56 -0400

    Update geoclient.py

--- API/geoclient.py
+++ API/geoclient.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+df = pd.read_excel('/Users/lordxuzhiyu/Desktop/Penthouse_Listing_SS.xlsx', sheetname = 'report generated at 0520pm on 2')
+df = df.drop([df.columns[0]], axis = 'columns')
+df = df.drop(df.index[[0,1,2]])
+df.columns = df.iloc[0]
+df = df.drop(df.index[0])
+df.reset_index(drop = True)
+
+df.to_csv('/Users/lordxuzhiyu/Desktop/Penthouse_Listing_SS.csv')
